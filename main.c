256deb3f480734575a38b48e8c4dbfc991ad6a2d
michinat <37168439+michinat@users.noreply.github.com>
2019-05-19 16:48:32 -0700

    customer thread

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <time.h> 
+#include <stdbool.h> 
 
 char* item[3] = {"Hamburger", "Fries", "Soda"};
 sem_t kitchen;
 int times = 0;
 int randNum,randNum1;
+bool chefDone, custDone, eating;
 
-void chefThread(void *arg)
+void *chefThread(void *arg)
 {
     srand(time(NULL));
     sem_wait(&kitchen);
@@ -26,14 +28,42 @@ void chefThread(void *arg)
             }
         }
         
-        printf("Chef cooks: %s and %s", item[randNum], item[randNum1]);
+        printf("Chef cooks: %s and %s\n", item[randNum], item[randNum1]);
+        chefDone = 1;
+        eating = 0;
 
         times++;
     }
     sem_post(&kitchen);
 }
 
+void *customerThread(void *arg)
+{
+    while(1)
+    {
+        if(!eating)
+        {
+            sem_wait(&kitchen);
+            if(chefDone && randNum != (int)arg && randNum1 != (int)arg)
+            {
+              printf("Customer %d completed eating\n", (int)arg);
+            }
+            chefDone = 0;
+            eating = 1;
+        }
+        sem_post(&kitchen);
+    }
+}
+
 int main(void)
 {
- sem_init (&kitchen, 0, 1);   
+    pthread_t cust1,cust2,cust3,chef;
+    sem_init (&kitchen, 0, 1);   
+    printf("Customer 1 has hamburger\n");
+    printf("Customer 2 has fries\n");
+    printf("Customer 3 has soda\n");
+    pthread_create(&chef,0,chefThread,0);
+    pthread_create(&cust1,0,customerThread,(void*)0);
+    pthread_create(&cust2,0,customerThread,(void*)1);
+    pthread_create(&cust3,0,customerThread,(void*)2);
 }
